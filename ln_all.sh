2ef9dfaf8ab2c3e811e6fdedf768a0931ce71bc9
juanvmarquezl <juanvmarquezl@gmail.com>
2017-01-16 17:00:35 -0400

    Update ln_all.sh file

--- ln_all.sh
+++ ln_all.sh
@@ -1,13 +1,15 @@
 echo 'Generando la carpeta modulos y agregar todos los enlaces simbólicos'
 
-cd /home/jmarquez/instancias/produccion/modulos/
+cd ~/instancias/produccion/modulos/
 
 echo 'tcv_municipal_tax'
-ln -s -f /home/jmarquez/github/openerp60/tcv_municipal_tax/ .
+ln -s -f ~/github/openerp60/tcv_municipal_tax/ .
 echo 'tcv_stock'
-ln -s -f /home/jmarquez/github/openerp60/tcv_stock/ .
+ln -s -f ~/github/openerp60/tcv_stock/ .
 echo 'tcv_mrp'
-ln -s -f /home/jmarquez/github/openerp60/tcv_mrp/ .
+ln -s -f ~/github/openerp60/tcv_mrp/ .
+echo 'tcv_account'
+ln -s -f ~/github/openerp60/tcv_account/ .
 
 
-cd /home/jmarquez/github/openerp60/
+cd ~/github/openerp60/
