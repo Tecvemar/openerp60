619b4cc241e9dc63d0cefc21f83ff32b3d90161d
Juan V. Márquez L <juanvmarquezl@gmail.com>
2018-10-17 17:06:37 -0400

    tcv_consignment_config

--- tcv_consignment/model/tcv_consignment_config.py
+++ tcv_consignment/model/tcv_consignment_config.py
@@ -36,6 +36,41 @@ class tcv_consignment_config(osv.osv):
     _columns = {
         'name': fields.char(
             'Name', size=64, required=False, readonly=False),
+        'partner_id': fields.many2one(
+            'res.partner', 'Partner', change_default=True,
+            readonly=False, required=True, ondelete='restrict'),
+        'stock_location_id': fields.many2one(
+            'stock.location', 'Stock location', change_default=True,
+            readonly=False, required=True, ondelete='restrict',
+            help="Destination location for material placed on consignment"),
+        'stock_journal_id': fields.many2one(
+            'stock.journal', 'Stock journal', change_default=True,
+            readonly=False, required=True, ondelete='restrict',
+            help="Stock journal to register moves"),
+        'inventory_account_id': fields.many2one(
+            'account.account', 'Inventory accoount', change_default=True,
+            readonly=False, required=True, ondelete='restrict',
+            help="Accounting account for the value of consigned inventory"),
+        'order_policy': fields.selection([
+            ('prepaid', 'Payment Before Delivery'),
+            ('manual', 'Shipping & Manual Invoice'),
+            ('postpaid', 'Invoice On Order After Delivery'),
+            ('picking', 'Invoice From The Picking'),
+            ], 'Shipping Policy', required=True, readonly=False,
+            help="""The Shipping Policy is used to synchronise invoice and
+            delivery operations.
+    - The 'Pay Before delivery' choice will first generate the invoice and then
+        generate the picking order after the payment of this invoice.
+    - The 'Shipping & Manual Invoice' will create the picking order directly
+        and wait for the user to manually click on the 'Invoice' button to
+        generate the draft invoice.
+    - The 'Invoice On Order After Delivery' choice will generate the draft
+        invoice based on sales order after all picking lists have been
+        finished.
+    - The 'Invoice From The Picking' choice is used to create an invoice during
+        the picking process."""),
+        'payment_term': fields.many2one(
+            'account.payment.term', 'Payment Term'),
         }
 
     _defaults = {
@@ -55,6 +90,7 @@ class tcv_consignment_config(osv.osv):
     ##----------------------------------------------------- create write unlink
 
     ##---------------------------------------------------------------- Workflow
+
 
 tcv_consignment_config()
 
